5dc36637e94dcf8dccf323aa5c586943685b8b46
Aidan Taylor <artaylor@cardiffmet.ac.uk>
2022-09-28 16:17:56 +0100

    Create neorandom.py

---
+++ neorandom.py
@@ -0,0 +1,26 @@
+import board # board module lets us use the hardware
+import neopixel # add the neopixel module
+from time import sleep # add the sleep module
+from random import randint # add the randint module
+
+# Update this to match the number of NeoPixel LEDs connected to your board.
+num_pixels = 5
+
+# Set up neopixel module with an object called 'pixels'
+pixels = neopixel.NeoPixel(board.GP22, num_pixels)
+
+# Set brightness between 0. and 1.
+pixels.brightness = 0.2
+
+while True:
+    # randint generates a random number in a set range:
+    R = randint(0,255)
+    G = randint(0,255)
+    B = randint(0,255)
+    x = R = randint(0,4)
+    
+    # Note how variables are used below:
+    pixels[x] = (R, G, B)
+    pixels.show()
+    
+    sleep(0.05)
